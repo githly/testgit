1ec0298d7ff4cea5095476f1bb1497419286fb9d
Lily <you@example.com>
2016-12-02 14:16:53 +0100

    index.js add

---
+++ index.js
@@ -0,0 +1,23 @@
+var fn_init = function () {
+
+}
+let lignes Cdes = [];
+let clients = [{name: 'client 1', code : "cl1"}
+                      { name : 'client2', code : 'cl2'} ];
+let TVA = [2, 10, 20.6];
+
+for (let i = 0 ; i < 1000 ; i ++) }
+    let idxTva = Math.round(
+                 Math.random()*2);
+    let myTva  = TVA[idxTva];
+    let idxCli =Math.round(Math.random()*5);
+    let myCli  = clients [idxCli];
+    let montHT = Math.random()*1000;
+
+lignesCdes.push(
+ [myCli, myTVA, montHT]);
+
+}
+console.log (fn_init);
+return lignesCdes;
+}
